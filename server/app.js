875f19caf3877c808a6223285ee6bb28a2c862a7
Nasa Quba <nasaquba@gmail.com>
2018-11-26 02:02:48 -0800

    added usernname input box - works

--- server/app.js
+++ server/app.js
@@ -15,3 +15,26 @@ fetchChatFromDB((chats) => {
     $('#messages').append($('<li>').text(chat.msg))
   })
 })
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then((registration) => {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch((err) => {
+    console.log('ServiceWorker registration failed: ', err);
+  })
+};
+
+var socket = io.connect('http://localhost');
+$(() => {
+  var socket = io();
+  $('form').submit(() => {
+    // send out value of the input box
+    socket.emit('chat msg', $('#m').val());
+    $('#m').val(''); // clear out input box after submitting text
+    return false;
+  })
+  socket.on('chat msg', function (msg) {
+    $('#messages').append($('<li>').text(msg))
+  });
+})
